b10644565ade8ef3055793648a15d9bc49b828f2
Carlos Chávez <u201910317@upc.edu.pe>
2024-06-09 15:56:56 -0500

    feat: navbar and routing added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Toolbar from "primevue/toolbar";
+import Avatar from "primevue/avatar";
+
+createApp(App)
+    .use(PrimeVue, {ripple: true})
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-avatar', Avatar)
+    .mount('#app')
